8838b330f418b5252c23a008d895ec25697edc23
kapil-varshney <kvarshney.14@gmail.com>
2018-03-29 15:11:29 +0530

    Added crop_image function

--- zoom/zoom_lists.py
+++ zoom/zoom_lists.py
@@ -22,8 +22,11 @@ def zoom_image(img, k):
     #Transpose the zoomed image in appropriate dimensions
     z_img_lt = [[[z_img_l[a][c][b] for a in range(channels)] for b in range(width*k)] for c in range(height*k)]
 
+    # Crop the image by calling the crop_image function
+    zoom_img = np.array(crop_image(z_img_lt, height, width, k, y, x))
+
     #Return the zoomed image in lists format
-    return z_img_lt
+    return zoom_img
 
 def pixel_rep(img_l, height, width, k=2):
     '''
@@ -50,6 +53,44 @@ def pixel_rep(img_l, height, width, k=2):
 
     return zoom_ch
 
+def crop_image(img_l, height, width, k, c_h, c_w):
+    '''
+    Function to crop the zoomed image to original image dimensions
+
+    img_l - zoom image in list format
+    height - height of the original Image
+    width- width of the original image
+    k - zoom/scale factor
+    c_h - height of pivot point (y axis)
+    c_w - width of pivot point (x axis)
+
+    Returns crop_imgl - cropped image in list format
+    '''
+
+    # Define the starting and ending positions for the crop
+    start_h = c_h*k - int(height/2)
+    end_h = c_h*k + int(height/2)
+    start_w = c_w*k - int(width/2)
+    end_w = c_w*k + int(width/2)
+
+    # Check if the starting points go out of range(<0)
+    if start_h < 0 : start_h = 0
+    if start_w < 0 : start_w = 0
+
+    crop_imgl = [[img_l[h][w] for w in range(start_w, end_w)] for h in range(start_h, end_h)]
+
+    '''
+    i = 0
+    j = 0
+    for h in range(start_h, end_h):
+        for w in range(start_w, end_w):
+            crop_imgl[i][j] = img_l[h][w]
+            j+=1
+        i+=1
+    '''
+
+    return crop_imgl
+
 
 if __name__ == '__main__':
 
@@ -69,9 +110,18 @@ if __name__ == '__main__':
     if scale<2:
         print ('Not a valid factor')
         exit()
+    y, x = list(map(int, input('Please enter the co-ordinates of the pivot point (space separated): ').split()))
+    if y<0 or y>img.shape[0] or x<0 or x>img.shape[1]:
+        print ('Pivot point outside image')
+        exit()
 
-    # Call the zoom_image function and get the zoomed image converted into ndarray
-    zoom_img = np.array(zoom_image(img, scale))
+    # Call the zoom_image function and get the zoomed image
+    zoom_img = zoom_image(img, scale)
+
+    #Save the image
+    print('Saving the image...')
+    misc.imsave('zoom.jpg', zoom_img)
+    print('Image saved as zoom.jpg')
 
     #Display the zoomed image
     fig.add_subplot(1,2,2)
